9e7ba7ba5601f5df401629831e31ee7b377bc325
Kemuntofaith <kemuntofaith20@gmail.com>
2022-09-07 02:39:15 -0700

    still trying

--- add.c
+++ add.c
@@ -1,28 +1,32 @@
-#include <stdio.h>
-#include <ctype.h>
-#include <stdlib.h>
-#include <string.h>
 #include "monty.h"
-
 /**
- * _add -  adds the first two nodes of the stack
- * @stack: stack given by main
- * @line_cnt: line counter
- *
- * Return: void
+ * _add - adds the top two elements of the stack.
+ * @stack: head
+ * @num_line: number of line
  */
-void _add(stack_t **stack, unsigned int line_cnt)
+void _add(stack_t **stack, unsigned int num_line)
 {
-	int result;
+	stack_t *temp1;
+	stack_t *temp2;
+	int sum = 0;
 
-	if (!stack || !*stack || !((*stack)->next))
+	if (*stack && (*stack)->next)
 	{
-		fprintf(stderr, "L%d: can't add, stack too short\n", line_cnt);
-		status = EXIT_FAILURE;
-		return;
+		temp2 = (*stack)->next;
+		sum = (*stack)->n + temp2->n;
+		temp1 = *stack;
+		*stack = (*stack)->next;
+		if (*stack)
+			(*stack)->prev = NULL;
+		free(temp1);
+		(*stack)->n = sum;
 	}
-
-	result = ((*stack)->next->n) + ((*stack)->n);
-	pop(stack, line_cnt);/*For top node*/
-	(*stack)->n = result;
+	else
+	{
+		dprintf (2, "L%d: can't add, stack too short\n", num_line);
+		free(global.line);
+		fclose(global.fil);
+		free_l(stack);
+		exit(EXIT_FAILURE);
+	}
 }
